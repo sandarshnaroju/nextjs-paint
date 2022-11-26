a79e69610f0af72ab519dcb9a079c972c77ff8d5
Sandarsh Naroju <narojusandarsh18@gmail.com>
2022-11-26 11:16:31 +0530

    firebase integration added

--- pages/api/socketio.ts
+++ pages/api/socketio.ts
@@ -1,33 +1,28 @@
-import { Server } from "socket.io";
+// import { Server } from "socket.io";
 
-const ioHandler = (res: any) => {
-  if (!res.socket.server.io) {
-    console.log("*First use, starting socket.io");
-
-    const io = new Server(res.socket.server);
-
-    io.on("connection", (socket) => {
-      socket.on("client-ready", () => {
-        socket.broadcast.emit("get-canvas-state");
-      });
-      socket.on("canvas-state", (state) => {
-        socket.broadcast.emit("canvas-state-from-server", state);
-      });
-
-      socket.on("draw-line", ({ prevPoint, currentPoint, color }) => {
-        socket.broadcast.emit("draw-line", { prevPoint, currentPoint, color }); // sending data to alla
-      });
-
-      socket.on("clear", () => {
-        io.emit("clear");
-      });
-    });
-
-    res.socket.server.io = io;
-  } else {
-    console.log("socket.io already running");
-  }
-  res.end();
+const ioHandler = (res) => {
+  // if (!res.socket.server.io) {
+  //   console.log("*First use, starting socket.io");
+  //   const io = new Server(res.socket.server);
+  //   io.on("connection", (socket) => {
+  //     socket.on("client-ready", () => {
+  //       socket.broadcast.emit("get-canvas-state");
+  //     });
+  //     socket.on("canvas-state", (state) => {
+  //       socket.broadcast.emit("canvas-state-from-server", state);
+  //     });
+  //     socket.on("draw-line", ({ prevPoint, currentPoint, color }) => {
+  //       socket.broadcast.emit("draw-line", { prevPoint, currentPoint, color }); // sending data to alla
+  //     });
+  //     socket.on("clear", () => {
+  //       io.emit("clear");
+  //     });
+  //   });
+  //   res.socket.server.io = io;
+  // } else {
+  //   console.log("socket.io already running");
+  // }
+  // res.end();
 };
 
 export const config = {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,58 +1,90 @@
 import { FC, useEffect, useRef, useState } from "react";
 import { useDraw } from "../hooks/useDraw";
 import { HexColorPicker } from "react-colorful";
+import { getDatabase, onValue, ref, set } from "firebase/database";
 import Head from "next/head";
-import io from "socket.io-client";
+
 import { DrawLine } from "../utils/DrawLine";
-
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 interface pageProps {}
-
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_apiKey,
+  authDomain: process.env.NEXT_PUBLIC_authDomain,
+  databaseURL: process.env.NEXT_PUBLIC_databaseURL,
+  projectId: process.env.NEXT_PUBLIC_projectId,
+  storageBucket: process.env.NEXT_PUBLIC_storageBucket,
+  messagingSenderId: process.env.NEXT_PUBLIC_messagingSenderId,
+  appId: process.env.NEXT_PUBLIC_appId,
+  measurementId: process.env.NEXT_PUBLIC_measurementId,
+};
 const Page: FC<pageProps> = ({}) => {
   const globalSocketRef: any = useRef(null);
   const { canvasRef, onMouseDown, onClear, saveAsImage } = useDraw(createLine);
   const [color, setColor] = useState("#0066a7");
 
   function createLine({ prevPoint, currentPoint, ctx }: Draw) {
-    if (globalSocketRef.current != null) {
-      globalSocketRef.current.emit("draw-line", {
-        prevPoint,
-        currentPoint,
-        color,
-      });
-      DrawLine({ prevPoint, currentPoint, ctx, color });
-    }
+    // if (globalSocketRef.current != null) {
+    // globalSocketRef.current.emit("draw-line", {
+    //   prevPoint,
+    //   currentPoint,
+    //   color,
+    // });
+    DrawLine({ prevPoint, currentPoint, ctx, color });
+    const db = getDatabase();
+    set(ref(db, "canvas"), {
+      prevPoint,
+      currentPoint,
+      ctx,
+      color,
+    });
+    // }
   }
+  // console.log("vall", JSON.parse(process.env.NEXT_PUBLIC_ENV_FirebaseConfig));
+
+  useEffect(() => {
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+    const database = getDatabase(app);
+    console.log("heeeelo");
+  }, []);
   useEffect(() => {
     const ctx = canvasRef.current?.getContext("2d");
-    fetch("/api/socketio").finally(() => {
-      const socket = io();
+    if (ctx) {
+      const db = getDatabase();
+      const starCountRef = ref(db, "canvas");
+      onValue(starCountRef, (snapshot) => {
+        const { prevPoint, currentPoint, color } = snapshot.val();
 
-      globalSocketRef.current = socket;
-      socket.emit("client-ready");
-
-      socket.on("get-canvas-state", () => {
-        if (!canvasRef.current?.toDataURL()) return;
-        socket.emit("canvas-state", canvasRef.current?.toDataURL());
-      });
-
-      socket.on("canvas-state-from-server", (state) => {
-        const img = new Image();
-        img.src = state;
-        img.onload = () => {
-          ctx?.drawImage(img, 0, 0);
-        };
-      });
-      socket.on("draw-line", ({ prevPoint, currentPoint, color }) => {
-        if (!ctx) {
-          return;
-        }
         DrawLine({ prevPoint, currentPoint, ctx, color });
       });
+    }
 
-      socket.on("clear", () => {
-        onClear();
-      });
-    });
+    // fetch("/api/socketio").finally(() => {
+    // const socket = io();
+    // globalSocketRef.current = socket;
+    // socket.emit("client-ready");
+    // socket.on("get-canvas-state", () => {
+    //   if (!canvasRef.current?.toDataURL()) return;
+    //   socket.emit("canvas-state", canvasRef.current?.toDataURL());
+    // });
+    // socket.on("canvas-state-from-server", (state) => {
+    //   const img = new Image();
+    //   img.src = state;
+    //   img.onload = () => {
+    //     ctx?.drawImage(img, 0, 0);
+    //   };
+    // });
+    // socket.on("draw-line", ({ prevPoint, currentPoint, color }) => {
+    //   if (!ctx) {
+    //     return;
+    //   }
+    //   DrawLine({ prevPoint, currentPoint, ctx, color });
+    // });
+    // socket.on("clear", () => {
+    //   onClear();
+    // });
+    // });
     return () => {
       if (!globalSocketRef.current && globalSocketRef.current != null) {
         globalSocketRef.current.off("get-canvas-state");
